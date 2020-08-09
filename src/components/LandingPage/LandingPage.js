f31a05ab4707ede034d758fb6e699a3b0c07e6bc
Hsin Ling Hu <hsinkava11@gmail.com>
2020-08-09 13:40:04 -0700

    adjust landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import landing from '../../image/landing.jpg'
+import landing2 from '../../image/landing2.jpg'
+import './LandingPage.css';
 
 export class LandingPage extends Component {
     render() {
         return (
             <div>
               <h1>We're here for you</h1>
-              <p>If you're a good listener or you're looking for a person to talk to, this is the place.
-                  mPath is a platform to have a warm conversation. 
+              <img className='landingImg' src={landing} alt="landing" />
+              <p>Sometimes, you just need to rant. Or cry. Or spill the tea. Whatever it is you need, we’re here for you, 
+                  all the time, every time. With personalized 1:1 attention and resources galore, we want to make sure you feel supported, 
+                  every step of the way.
               </p>
-              <Link to='/register'>Join us</Link>
+              <h2>Join our community to share your story or support someone by listening</h2>
+              <Link to='/register'>Join Now</Link>
+              <h2>Beyond the Talk</h2>
+              <img  className='landing2' src={landing2} alt="landing2" />
+              <p >If you loved the one time talk with a stranger and would like to continue improving your mental health journey, 
+                  check out our recurring resources below!</p>
               
             </div>
         )
